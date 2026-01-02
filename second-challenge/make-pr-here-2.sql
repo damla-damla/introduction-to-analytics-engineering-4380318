6ec9158dc5826f12b3a6acbfe9a36263ad0f53cd
damla-damla <damlademir15@gmail.com>
2026-01-02 00:14:16 -0500

    Modify product_perf view to add total_no_states
    Updated the view to include total number of distinct states.

--- second-challenge/make-pr-here-2.sql
+++ second-challenge/make-pr-here-2.sql
@@ -1,10 +1,12 @@
 
-Create view product_perf as
-    SELECT p.product_id, p.productname, p.productcategory , p.price, sum(o.quantity) as total_prod_sold, count(o.order_id) as total_prod_orders
+Create or replace view product_perf as
+    SELECT p.product_id, p.productname, p.productcategory , p.price, sum(o.quantity) as total_prod_sold, count(o.order_id)  as total_prod_orders, count(distinct c.state) as total_no_states
 
 FROM "INTRO_TO_ANALYTICS_ENG"."PUBLIC"."PRODUCT" p
-LEFT JOIN "INTRO_TO_ANALYTICS_ENG"."PUBLIC"."ORDERS" o
+JOIN "INTRO_TO_ANALYTICS_ENG"."PUBLIC"."ORDERS" o
     on p.product_id=o.productid
+JOIN "INTRO_TO_ANALYTICS_ENG"."PUBLIC"."CUSTOMERS" c
+    on c.cust_id=o.cust_id
 group by p.product_id, p.productname, p.productcategory, p.price
 
 select * from product_perf
